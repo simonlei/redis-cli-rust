06062c8664b8c3c39acc27da983f2291c5c3a6d0
simonlei <simonlei@tencent.com>
2022-12-19 15:48:03 +0800

    Can read from input.

--- TODO.md
+++ TODO.md
@@ -1,4 +1,8 @@
 # TODO
 
-- [ ] read line from input as input
-- [ ] print response to output 
+- [x] read line from input as input
+- [ ] print response to output
+
+- [ ] Advanced Feature: add tab support at input
+- [ ] Advanced Feature: parse command line params
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate core;
+
+use std::{env, io};
+use std::io::Write;
+
+struct RedisContext {
+    ip: String,
+    port: u16,
+    auth: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: parse command line to get redis context
+    let redis_context = RedisContext {
+        ip: String::from("9.134.105.13"),
+        port: 6380,
+        auth: env::var("AUTH").unwrap(),
+    };
+
+    loop {
+        print!("{}:{}>", redis_context.ip, redis_context.port);
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        input = input[..input.len() - 1].to_string();
+        if input.eq("quit") { std::process::exit(0); }
+        println!("Hello, world! {input}");
+    }
 }
